5f790620593dc6dd950ead93fd83bf92fa3b9dfa
Carol Andrade Santos <carolandrades11@gmail.com>
2021-11-07 10:45:08 -0300

    feat(body): create body for the index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Avatar from '../src/components/avatar';
-import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
+import { ViewGridIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
 
 export default function Home() {
   return (
@@ -12,11 +14,7 @@ export default function Home() {
       </Head>
 
       <header>
-        <nav className="flex w-full p-5 justify-between text-sm text-gray-700">
-          <ul className="flex space-x-4 items-center">
-            <li className="link">About</li>
-            <li className="link">Store</li>
-          </ul>
+        <nav className="flex w-full p-5 justify-end text-sm text-gray-700">
           <ul className="flex space-x-4 items-center">
             <li className="link">Gmail</li>
             <li className="link">Images</li>
@@ -25,6 +23,34 @@ export default function Home() {
           </ul>
         </nav>
       </header>
+
+      <form className="flex flex-col items-center mt-28 flex-grow">
+        <h1>
+          <Image
+            src="https://www.google.com/logos/doodles/2021/halloween-2021-6753651837109122.2-2xa.gif"
+            alt="Google Logo"
+            width={340}
+            height={120}
+          />
+        </h1>
+
+        <div className="flex w-full mt-5 shadow-md focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-2.5 items-center sm:max-w-xl">
+          <SearchIcon className="h-4 mr-3 text-gray-500" />
+          <input type="text" placeholder="Search Google or type a URL" className="flex-grow focus:outline-none" />
+          <Image 
+            src="https://seeklogo.com/images/G/google-mic-logo-33133A4F5F-seeklogo.com.png"
+            alt="Google microphone"
+            title="Search by voice"
+            width={13}
+            height={19}
+          />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I{"'"}m Feeling Lucky</button>
+        </div>
+      </form>
     </div>
   )
 }
