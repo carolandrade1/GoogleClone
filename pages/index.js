0c9e7a520a932a34795a03aeb899a2ea6b23d8ec
Carol Andrade Santos <carolandrades11@gmail.com>
2021-11-13 16:24:33 -0300

    feat(router): add router to search page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
-import Head from 'next/head';
+import { useRef } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Avatar from '../src/components/avatar';
 import { ViewGridIcon } from '@heroicons/react/solid';
 import { SearchIcon } from '@heroicons/react/outline';
 import Footer from '../src/components/footer';
+import IndexHead from '../src/components/common/head';
 
 export default function Home() {
+
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+  
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if(!term) return;
+
+    router.push(`/search?term=${term}`)
+  }
+  
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Head>
-        <title>Google</title>
-        <meta name="description" content="Google Clone made with next" />
-        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png" />
-      </Head>
+      <IndexHead title='Google' />
 
       <header className="w-full">
         <nav className="flex w-full p-5 justify-end text-sm text-gray-700">
@@ -25,19 +36,19 @@ export default function Home() {
         </nav>
       </header>
 
-      <form className="flex flex-col items-center mt-28 flex-grow px-2 md:w-4/5">
+      <form className="flex flex-col items-center mt-12 flex-grow px-2 md:w-4/5">
         <h1>
           <Image
-            src="https://www.google.com/logos/doodles/2021/halloween-2021-6753651837109122.2-2xa.gif"
+            src="https://www.loosebyte.com/wp-content/uploads/2020/10/google-logo-transparent.png"
             alt="Google Logo"
-            width={340}
-            height={120}
+            width={310}
+            height={105}
           />
         </h1>
 
         <div className="flex w-full mt-5 shadow-md focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-2.5 items-center sm:max-w-xl">
           <SearchIcon className="h-4 mr-3 text-gray-500" />
-          <input type="text" placeholder="Search Google or type a URL" className="flex-grow focus:outline-none" />
+          <input ref={searchInputRef} type="text" placeholder="Search Google or type a URL" className="flex-grow focus:outline-none" />
           <Image 
             src="https://seeklogo.com/images/G/google-mic-logo-33133A4F5F-seeklogo.com.png"
             alt="Google microphone"
@@ -48,8 +59,8 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Google Search</button>
-          <button className="btn">I{"'"}m Feeling Lucky</button>
+          <button onClick={search} className="btn">Google Search</button>
+          <button onClick={search} className="btn">I{"'"}m Feeling Lucky</button>
         </div>
       </form>
 
