214357c72b4a9c3f5d36837f9fa12b7494336210
Carol Andrade Santos <carolandrades11@gmail.com>
2021-11-20 15:10:55 -0300

    feat(heroheader): create heroHeader components

---
+++ src/components/heroHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Avatar from '../components/avatar';
+import { ViewGridIcon } from '@heroicons/react/solid';
+
+function HeroHeader() {
+  return (
+    <header className="w-full">
+      <nav className="flex w-full p-5 justify-end text-sm text-gray-700">
+        <ul className="flex space-x-4 items-center">
+          <li className="link">Gmail</li>
+          <li className="link">Images</li>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="https://avatars.githubusercontent.com/u/65976843?v=4" />
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default HeroHeader
