d940ee975a54c7f8736d7690c7dae0bdc55acbc4
Carol Andrade Santos <carolandrades11@gmail.com>
2021-11-20 14:51:20 -0300

    refactor: small changes

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-function Avatar({ url }) {
+function Avatar({ url, className }) {
   return (
-    <img
-      src={url}
-      loading="lazy"
-      alt="Profile picture"
-      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
-    />
+    <a href="https://github.com/carolandrade1" target="_blank" rel="noopener noreferrer" title="Author Github">
+      <img
+        src={url}
+        loading="lazy"
+        alt="Author Github"
+        className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      />
+    </a>
   );
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,7 @@ function Footer() {
       <div className="px-8 py-3">
         <p>Brazil</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-2 grid-flow-row-dense px-8 py-3">
-        <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-          <GlobeIcon className="h-5 mr-1 text-green-700" />
-          Carbon neutral since 2007
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2 grid-flow-row-dense px-8 py-3">
         <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
           <p>Advertising</p>
           <p>Business</p>
