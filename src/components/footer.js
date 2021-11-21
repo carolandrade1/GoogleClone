02fc6adee03ef8954f98824a969fb9a4e08787fb
Carol Andrade Santos <carolandrades11@gmail.com>
2021-11-21 08:57:39 -0300

    fix(accessibility): fix accessibility problems

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import { GlobeIcon } from "@heroicons/react/solid";
 
 function Footer() {
   return (
-    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
+    <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-600">
       <div className="px-8 py-3">
         <p>Brazil</p>
       </div>
